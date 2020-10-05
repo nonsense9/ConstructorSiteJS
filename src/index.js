f6fb67c12f9eb1b66aaa920948944a68e6efdd17
Alexey Ceaicovschiy <alexeyc3991@gmail.com>
2020-10-05 13:23:30 +0300

    First project

---
+++ src/index.js
@@ -0,0 +1,74 @@
+import {model} from './model'
+import {App} from './classes/app'
+import './styles/main.css'
+
+new App(model).init()
+
+
+// const model = [
+//   {type: 'title', value: 'Hey there'},
+//   {type: 'text', value: 'here is the text'},
+//   {type: 'columns', value: [
+//     '111',
+//     '222',
+//     '333',
+//   ]},
+//   {type: 'image', value: './assets/image.png'}
+// ]
+
+// const $site = document.querySelector('#site')
+
+// model.forEach(block => {
+//   let html = ''
+//   if (block.type === "title") {
+//     html = title(block);
+//   } else if (block.type === "text") {
+//     html = text(block);
+//   } else if (block.type === "columns") {
+//     html = columns(block);
+//   } else if (block.type === "image") {
+//     html = image(block);
+//   }
+
+//   $site.insertAdjacentHTML('beforeend', html)
+// })
+
+// function title(block) {
+//   return `
+//   <div class="row">
+//   <div class="col-sm">
+//     <h1>
+//       ${block.value}
+//     </h1>
+//   </div>
+// </div>
+// `
+// }
+
+// function text(block) {
+//   return `
+//   <div class="row">
+//   <div class="col-sm">
+//     <p>
+//       ${block.value}
+//     </p>
+//   </div>
+//   `
+// }
+
+// function columns(block) {
+//   const html = block.value.map(item => `<div class ="col-sm">${item}</div>`)
+//   return `
+//   <div class="row">
+//   ${html.join('')}
+//   </div>
+//   `
+// }
+
+// function image(block) {
+//   return `
+//   <div class = "row"
+//   <img src="${block.value}" />
+//   </div>
+//   `
+// }
---
+++ src/model.js
@@ -0,0 +1,51 @@
+import image from './assets/image.png'
+import {TextBlock, TitleBlock, ColumnsBlock, ImageBlock} from './classes/blocks'
+
+// const text = `
+// Крутые видео и уроки по JavaScript тут: <a href="https://youtube.com/c/VladilenMinin" target="_blank">Владилен Минин</a>. Тут ты найдешь исчерпывающую информацию по любым аспектам языка, любым фреймворкам, такие как: React, Vue, Angular, Node, Svelte, Express, Next, Nuxt и многое другое. Присоединяйся!
+// `
+const text = ''
+
+
+export const model = [
+  new TitleBlock('Конструктор сайтов на чистом JavaScript', {
+    tag: 'h2',
+    styles: {
+      background: 'linear-gradient(to right, #ff0099, #493240)',
+      color: 'red',
+      padding: '1.5rem',
+      'text-align': 'center'
+    }
+  }),
+  new ImageBlock(image, {
+    styles: {
+      padding: '2rem 0',
+      display: 'flex',
+      'justify-content': 'center'
+    },
+    imageStyles: {
+      width: '500px',
+      height: 'auto'
+    },
+    alt: 'Это картинка'
+  }),
+  new ColumnsBlock([
+    'Приложение на чистом JavaScript, без использования библиотек',
+    'Построенно по принципу ООП',
+    'JavaScript - это просто и интересно.'
+  ], {
+    styles: {
+      background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
+      padding: '2rem',
+      color: '#fff',
+      'font-weight': 'bold'
+    }
+  }),
+  new TextBlock(text, {
+    styles: {
+      background: 'linear-gradient(to left, #f2994a, #f2c94c)',
+      padding: '1rem',
+      'font-weight': 'bold'
+    }
+  })
+]
